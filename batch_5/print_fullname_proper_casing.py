2ca02987283428d2c8cfd7e813510df917fe431f
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 18:02:51 +0800

    Capitalized first letter of each word, and put the words back in one string

--- batch_5/print_fullname_proper_casing.py
+++ batch_5/print_fullname_proper_casing.py
@@ -3,5 +3,7 @@ fullname = input("Enter your name in incorrect casing: ")
 #split name into words
 name_parts = fullname.split()
 #capitalize only first letter of the word
+capitalized_letter = [word.capitalize() for word in name_parts]
 #put words back in a single string
+proper_casing = " ".join(capitalized_letter)
 #print output

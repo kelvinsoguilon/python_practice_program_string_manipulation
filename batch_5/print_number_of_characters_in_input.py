82aff15b891215cd4ddc89a73f030a24586bc0db
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:01:39 +0800

    Ask user for fullname, Added variable to store count of characters

--- batch_5/print_number_of_characters_in_input.py
+++ batch_5/print_number_of_characters_in_input.py
@@ -1,4 +1,6 @@
 #ask user for fullname
+fullname = input("Enter your fullname: ")
 #store count of every character in fullname
+character_count = 0
 #check every character in input and adds it to count
 #print result

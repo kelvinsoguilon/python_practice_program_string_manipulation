af8b58a4221c0d17ee783f68c7c0f1432f7e2229
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 17:00:05 +0800

    Added user input, and checks whether input is in range

--- batch_5/add_zero_until_six_digit.py
+++ batch_5/add_zero_until_six_digit.py
@@ -1,4 +1,6 @@
 #ask user to input number from 0-1000
+num = input("Enter a number from 0-1000: ")
 #check if num is within range
+if num.isdigit and 0 <= int(num) <= 1000:
 #count how many 0 to add
 #print output

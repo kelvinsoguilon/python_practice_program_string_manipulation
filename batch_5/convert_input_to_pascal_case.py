d37f10da1bf23dbfce2b03a7f654202de24a0547
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:27:04 +0800

    Split the string into words and Capitalize the first letter of each word

--- batch_5/convert_input_to_pascal_case.py
+++ batch_5/convert_input_to_pascal_case.py
@@ -3,6 +3,8 @@ fullname = input("Enter your name in incorrect casing: ")
 #store characters in variable
 pascal_case = ""
 #split the string into words
+capitalized_letter = fullname.split()
 #capitalize the first letters of each word
+proper_case = [word.capitalize() for word in capitalized_letter]
 #remove the spaces
 #print result in pascal casing

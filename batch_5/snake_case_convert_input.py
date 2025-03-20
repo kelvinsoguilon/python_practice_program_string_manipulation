1792f260f084106c692a7532490ad3128eaedb1e
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:37:45 +0800

    Converts input to lowercase

--- batch_5/snake_case_convert_input.py
+++ batch_5/snake_case_convert_input.py
@@ -1,5 +1,6 @@
 #ask user for name in incorrect casing
 fullname = input("Enter your name in incorrect casing: ")
 #convert characters to lowercase
+lowercased_fullname = fullname.lower()
 #change spaces to underscore
 #print result

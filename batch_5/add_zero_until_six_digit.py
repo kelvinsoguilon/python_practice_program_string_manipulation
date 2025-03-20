3d57574288f183f8a88d4bcb585783fcb4d8bf45
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 17:01:45 +0800

    Checks how many zeros that needs to be added in the beginning

--- batch_5/add_zero_until_six_digit.py
+++ batch_5/add_zero_until_six_digit.py
@@ -3,4 +3,6 @@ num = input("Enter a number from 0-1000: ")
 #check if num is within range
 if num.isdigit and 0 <= int(num) <= 1000:
 #count how many 0 to add
+    number_zeros = 6 - len(num)
+    new_num = "0" * number_zeros + num
 #print output

43d67d6bc82ae2b48f79d3845f6f29aa66fe3314
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 15:38:45 +0800

    Print output

--- batch_5/print_fullname_without_leading_spaces.py
+++ batch_5/print_fullname_without_leading_spaces.py
@@ -1,12 +1,13 @@
 #string input
-full_name = ("Enter your name with leading spaces: ")
+full_name = input("Enter your name with leading spaces: ")
 #variable to store new characters without space
-fullname_without_space = ""
+full_name_without_space = ""
 #space before text checker
 space_found = True
 #iterate every text character to identify spaces
 for char in full_name:
     if char != " " or not space_found: #adds character that is not a space 
-        fullname_without_space += char
+        full_name_without_space += char
         space_found = False 
 #print output
+print(full_name_without_space)

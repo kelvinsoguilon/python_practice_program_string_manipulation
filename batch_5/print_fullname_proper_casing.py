69e069e96d84682b262c23e7cca54767072467a3
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 17:55:54 +0800

    Ask user for full name in incorrect casing, and splits input into words

--- batch_5/print_fullname_proper_casing.py
+++ batch_5/print_fullname_proper_casing.py
@@ -1,5 +1,7 @@
 #ask user for fullname
+fullname = input("Enter your name in incorrect casing: ")
 #split name into words
+name_parts = fullname.split()
 #capitalize only first letter of the word
 #put words back in a single string
 #print output

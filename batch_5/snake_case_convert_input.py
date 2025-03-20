d129dc8728bc280f0996251cf42752d352650e4e
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:38:34 +0800

    Change spaces to underscores

--- batch_5/snake_case_convert_input.py
+++ batch_5/snake_case_convert_input.py
@@ -3,4 +3,5 @@ fullname = input("Enter your name in incorrect casing: ")
 #convert characters to lowercase
 lowercased_fullname = fullname.lower()
 #change spaces to underscore
+snake_case = lowercased_fullname.replace(" ", "_")
 #print result

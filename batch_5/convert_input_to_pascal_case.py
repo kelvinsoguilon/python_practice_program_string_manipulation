a7cec6853ec1c5d15447bad459edecee80dace34
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:25:18 +0800

    Ask user for name in incorrect casing

--- batch_5/convert_input_to_pascal_case.py
+++ batch_5/convert_input_to_pascal_case.py
@@ -1,5 +1,7 @@
 #ask user for fullname
+fullname = input("Enter your name in incorrect casing: ")
 #store characters in variable
+pascal_case = ""
 #split the string into words
 #capitalize the first letters of each word
 #remove the spaces

b5dc251b8d7918836b46614925d0787dde5c858b
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 18:27:56 +0800

    Reverse case of each letter and join words into one string

--- batch_5/reverse_case_fullname_input.py
+++ batch_5/reverse_case_fullname_input.py
@@ -1,5 +1,7 @@
 #ask user for full name
 fullname = input("Enter your name in incorrect casing: ")
 #reverse case of each letter
+reversed_case = [char.upper() if char.islower() else char.lower() for char in fullname]
 #join words into one string
+fullname_reversed = "".join(reversed_case)
 #print result

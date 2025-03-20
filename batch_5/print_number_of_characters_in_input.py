7792161d0768c19b42177bbc14a46cf80af0cbd7
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-03-20 19:02:36 +0800

    Checks every character in fullname and adds it to count

--- batch_5/print_number_of_characters_in_input.py
+++ batch_5/print_number_of_characters_in_input.py
@@ -3,4 +3,6 @@ fullname = input("Enter your fullname: ")
 #store count of every character in fullname
 character_count = 0
 #check every character in input and adds it to count
+for char in fullname:
+    character_count += 1
 #print result
